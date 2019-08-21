a804865cb502e74a271e32a8b42bff1cccbd6844
unknown <crazdave@gmail.com>
2019-08-20 23:56:54 -0700

    header stuff

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <div class="bg-black">
+      <div class="py-2 container flex mx-auto">
+        <div class="ml-auto px-3">F</div>
+        <div class="px-3">I</div>
+        <button class="px-3">Book an Event</button>
+      </div>
+    </div>
+    <div class="bg-faded absolute inset-x-0">
+      <nav class="container flex mx-auto">
+        <ul class="flex w-2/5">
+          <li class="text-center my-auto w-1/2">Eat & Drink</li>
+          <li class="text-center my-auto w-1/2">Parties & Events</li>
+        </ul>
+        <div class="w-1/5" style="height: 150px">Logo</div>
+        <ul class="flex w-2/5">
+          <li class="text-center my-auto w-1/2">Attractions</li>
+          <li class="text-center my-auto w-1/2">Leagues</li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style>
+.bg-faded {
+  background-color: rgba(100, 150, 150, 0.5);
+}
+</style>
